ef37ad19c28c4f7e04d4d613ab10463f8900f678
mike2018 <wdmwdm2008@gmail.com>
2019-04-25 14:28:09 -0600

    adding cache-demo example by using springboot's inbuilt cache abstraction

---
+++ springboot with NoSQL/cache-demo/src/main/java/com/bigAlsTexture/SpringBucks/SpringBucksApplication.java
@@ -0,0 +1,42 @@
+package com.bigAlsTexture.SpringBucks;
+
+import com.bigAlsTexture.SpringBucks.service.CoffeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories
+@EnableCaching(proxyTargetClass = true)
+@Slf4j
+public class SpringBucksApplication implements ApplicationRunner {
+
+	@Resource
+	CoffeeService coffeeService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBucksApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("Count: {}", coffeeService.findAllCoffee().size());
+
+		for(int i = 0; i < 10; i++){
+			log.info("reading from cache");
+			coffeeService.findAllCoffee();
+		}
+
+		coffeeService.reloadCoffee();
+		log.info("Reading after refresh!");
+		coffeeService.findAllCoffee().forEach(c -> log.info("Coffee: {}", c.getName()));
+	}
+}
