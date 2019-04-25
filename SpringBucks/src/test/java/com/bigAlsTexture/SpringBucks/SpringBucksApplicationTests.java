1433f3ca6f5a1278d04fd19c9ff2ae1498b4b432
mike2018 <wdmwdm2008@gmail.com>
2019-04-25 16:54:55 -0600

    go back

--- SpringBucks/src/test/java/com/bigAlsTexture/SpringBucks/SpringBucksApplicationTests.java
+++ SpringBucks/src/test/java/com/bigAlsTexture/SpringBucks/SpringBucksApplicationTests.java
@@ -7,21 +7,12 @@ import com.bigAlsTexture.SpringBucks.entity.OrderState;
 import com.bigAlsTexture.SpringBucks.service.CoffeeOrderService;
 import com.bigAlsTexture.SpringBucks.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.money.CurrencyUnit;
-import org.joda.money.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 import javax.annotation.Resource;
-import java.util.Map;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
@@ -38,11 +29,6 @@ public class SpringBucksApplicationTests {
 	@Resource
 	private CoffeeService coffeeService;
 
-	@Resource
-	private JedisPoolConfig jedisPoolConfig;
-	@Resource
-	private JedisPool jedisPool;
-
 	@Test
 	public void contextLoads() {
 		log.info("All Coffee: {}", coffeeDao.findAll());
@@ -52,23 +38,6 @@ public class SpringBucksApplicationTests {
 			CoffeeOrder order = coffeeOrderService.createOrder("Li Lei", latte.get());
 			log.info("Update INIT to PAID: {}", coffeeOrderService.updateState(order, OrderState.PAID));
 			log.info("Update PAID to INIT: {}", coffeeOrderService.updateState(order, OrderState.INIT));
-		}
-	}
-
-	@Test
-	public void contextLoads1() {
-		log.info(jedisPoolConfig.toString());
-		try(Jedis jedis = jedisPool.getResource()){
-			coffeeService.findAllCoffee().forEach(c -> {
-				jedis.hsetnx("springbucks-menu",
-					c.getName(),
-					Long.toString(c.getPrice().getAmountMinorLong()));
-			});
-			Map<String, String> menu = jedis.hgetAll("springbucks-menu");
-			log.info("Menu: {}", menu);
-
-			String price = jedis.hget("springbucks-menu", "espresso");
-			log.info("espresso: {}", Money.ofMinor(CurrencyUnit.of("CNY"), Long.parseLong(price)));
 		}
 	}
 }
